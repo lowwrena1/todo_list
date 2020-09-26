d1658bf319f1b3b785ff72f4c7088460e5a3525b
Lorena Ontiveros <lorena0328o@gmail.com>
2020-09-26 11:38:50 -0500

    added completed and delete buttons through javascript to the todo task created by user

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,5 +16,19 @@ function addTodo(event){
     todoDiv.classList.add('todo');
     //Create LI
     const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"><i>'
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //check delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fas fa-delete"><i>'
+    deleteButton.classList.add("delete-btn");
+    todoDiv.appendChild(DeleteButton);
+
     console.log('test1');
 }
